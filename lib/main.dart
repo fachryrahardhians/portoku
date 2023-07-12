c7cb3e8051c0f11fb6014a41745fa1b421e6baa8
Fachry Rahardhian's <fachrysaputra2@gmail.com>
2023-07-12 22:49:34 +0700

    add firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:portoku/configs/app.dart';
+import 'package:portoku/firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(
+    const MainApp(),
+  );
 }
-
-
